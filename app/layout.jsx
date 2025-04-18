c38ff1b4ba93b44ebfa8fbe13f938165d3d57ba5
KoiralaSam <samarpankoirala@gmail.com>
2025-04-18 17:29:20 -0500

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+import "@/assets/styles/global.css";
+export const metadata = {
+  title: "Property Pulse",
+  keywords: "rental, property, real estate",
+  description: "find the perfect rental property",
+};
+const MainLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
